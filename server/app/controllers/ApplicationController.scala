05545f3f00a5da9a03a167452db0059e779f11ef
Taylor Ninesling <t9sling@hotmail.com>
2017-04-09 05:49:30 -0400

    Made file upload API calls asynchronous Action

--- server/app/controllers/ApplicationController.scala
+++ server/app/controllers/ApplicationController.scala
@@ -3,6 +3,7 @@ package controllers
 import services.ClarifaiService
 import services.FoursquareService
 
+import akka.actor.ActorSystem
 import java.io.File
 import javax.inject._
 import play.api._
@@ -11,29 +12,35 @@ import play.api.data.Forms._
 import play.api.mvc._
 import play.api.i18n.I18nSupport
 import play.api.i18n.MessagesApi
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.duration._
 
 @Singleton
 class ApplicationController @Inject()(val messagesApi: MessagesApi,
-    fs: FoursquareService, cs: ClarifaiService) extends Controller with I18nSupport {
+    fs: FoursquareService, cs: ClarifaiService, actorSystem: ActorSystem)
+    (implicit exec: ExecutionContext) extends Controller with I18nSupport {
 
   def uploadForm = Action {
     Ok(views.html.form())
   }
 
-  def upload = Action { implicit request =>//(parse.multipartFormData) { implicit request =>
-    /*val formData = request.body.asFormUrlEncoded
+  def upload = Action.async(parse.multipartFormData) { implicit request =>
+    val formData = request.body.asFormUrlEncoded
     val lat = parseDouble(formData.get("lat"))
     val lon = parseDouble(formData.get("lon"))
-    val trainingPhotos = fs.venuePhotos(lat, lon)
 
-    println(request.body)
+    request.body.file("image").map {
+      image => getFuturePrediction(lat,lon,image.ref.file).map {
+        prediction => Ok(prediction)
+      }
+    }.get
+  }
 
-    request.body.file("image").map { image =>
-      val imageFile = image.ref.file
-      val prediction = cs.run(trainingPhotos, imageFile)
-      Ok(prediction)
-    }.get*/
-    Ok(request.body.toString)
+  private def getFuturePrediction(lat: Double, lon: Double, image: File): Future[String] = {
+    Future {
+      val trainingPhotos = fs.venuePhotos(lat, lon)
+      cs.run(trainingPhotos, image)
+    }
   }
 
   def parseDouble(os: Option[Seq[String]]): Double =
